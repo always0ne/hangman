d6a720e2b670bfdb1e56ff2b6096a8332216b9fe
always0ne <always0ne.dev@gmail.com>
2021-06-03 04:01:23 +0900

    docs: 주석 추가

--- src/hangman/HangManView.java
+++ src/hangman/HangManView.java
@@ -16,6 +16,14 @@ public class HangManView extends JFrame {
     private final ScoreBoard scoreBoard;
     private final GuessPanel guessPanel;
 
+    /***
+     * HangMan 게임의 화면을 담당한다.
+     * 생성자에서는 화면을 구성하는데 필요한 요소들을 초기화 하는 과정을 거친다.
+     * HiddenString객체는 문제 문자열을 표시
+     * HangManImage 객체는 이미지를 표시
+     * ScoreBoard 객체는 점수판을 표시
+     * GuessPanel은 키 입력을 받는 부분을 표시한다.
+     */
     HangManView() {
         setSize(520, 700);
         setTitle("Hangman");
@@ -44,29 +52,58 @@ public class HangManView extends JFrame {
         setVisible(true);
     }
 
+    /***
+     * 키 입력의 이벤트리스너를 컨트롤러에서 등록하기 위해 생성한 getter 이다.
+     * @return 키 입력 받는 패널
+     */
     public GuessPanel getGuessPanel() {
         return guessPanel;
     }
 
+    /***
+     * 새로운 단어가 초기화될때 화면을 새로 그리는 기능을 한다.
+     * 키 입력을 받는 guessPanel의 상태를 전부 초기화 한 후, 문자열을 업데이트 한다.
+     * @param hiddenString 단어가 숨겨진 문자열
+     */
     public void initNewWord(String hiddenString) {
         guessPanel.reset();
         this.hiddenString.updateHiddenString(hiddenString);
     }
 
+    /***
+     * 카운트 정보를 업데이트 한다.
+     * 접수판에 점수를 업데이트 요청을 한 후, 이미지 변경 요청을 한다.
+     * @param dto 카운트 정보를 가지고 있는 Data Transfer Object
+     */
     public void updateCount(CountDto dto) {
         scoreBoard.updateScoreBoard(dto);
         hangmanImage.updateStep(dto.getWrongCount());
     }
 
+    /***
+     * 누른 키가 정답이었을 때 업데이트된 문자열을 갱신하며,
+     * 키 입력을 받는 guessPanel에 맞는 키 표시를 요청한다.
+     * @param maskingAnswer 업데이트 된 문자열
+     * @param pressedButton 변경할 버튼
+     */
     public void updateCorrect(String maskingAnswer, JButton pressedButton) {
         hiddenString.updateHiddenString(maskingAnswer);
         guessPanel.setCorrectKey(pressedButton);
     }
 
+    /***
+     * 누른 키가 정답이 아닐 때 guessPanel에 틀린 키 표시를 요청한다.
+     * @param pressedButton 변경할 버튼
+     */
     public void updateWrong(JButton pressedButton) {
         guessPanel.setWrongKey(pressedButton);
     }
 
+    /***
+     * 사용자에게 간단한 알림창을 제공한다.
+     * @param title 창 제목
+     * @param message 창 메시지
+     */
     public void alert(String title, String message) {
         JOptionPane.showMessageDialog(
                 this, message,
--- src/hangman/component/GuessPanel.java
+++ src/hangman/component/GuessPanel.java
@@ -10,6 +10,10 @@ import javax.swing.JPanel;
 public class GuessPanel extends JPanel {
     public static JButton[] letters = new JButton[52];
 
+    /***
+     * 키 입력을 받는 화면이다.
+     * a부터 z까지 버튼을 생성한다.
+     */
     public GuessPanel() {
         int k = 0;
         for (char i = 'a'; i <= 'z'; i++) {
@@ -23,15 +27,22 @@ public class GuessPanel extends JPanel {
         this.setOpaque(true);
     }
 
-    public void setKeyAction(ActionListener a) {
+    /***
+     * 모든 키 버튼에 액션리스너를 등록한다.
+     * @param actionListener 액션 리스너
+     */
+    public void setKeyAction(ActionListener actionListener) {
         try {
             for (int i = 0; i < 26; i++) {
-                letters[i].addActionListener(a);
+                letters[i].addActionListener(actionListener);
             }
         } catch (NullPointerException ignored) {
         }
     }
 
+    /***
+     * 버튼의 배경색을 정상으로 바꾼 후, 비활성화된 버튼을 전부 다시 활성화 한다.
+     */
     public void reset() {
         try {
             for (int i = 0; i < 26; i++) {
@@ -42,11 +53,19 @@ public class GuessPanel extends JPanel {
         }
     }
 
+    /***
+     * 정답인 키는 초록색으로 색을 변경하며, 비활성화 한다.
+     * @param pressedButton 정답인 키 버튼
+     */
     public void setCorrectKey(JButton pressedButton) {
         pressedButton.setBackground(Color.GREEN);
         pressedButton.setEnabled(false);
     }
 
+    /***
+     * 틀린 키는 빨간색으로 색을 변경하며, 비활성화 한다.
+     * @param pressedButton 실패한 버튼
+     */
     public void setWrongKey(JButton pressedButton) {
         pressedButton.setBackground(Color.RED);
         pressedButton.setEnabled(false);
